2b86f7a21a8b24252b41d31de02f20a3800fa980
Yue Dong <ydong011@fiu.edu>
2024-06-04 14:57:29 -0500

    update

--- js/index.js
+++ js/index.js
@@ -173,7 +173,10 @@
 // };
 
 // function Employee(name, age, company) {
-// 	Person.call(this, name, age); // * <----------- call, apply, bind
+// 	Person.call(this, name, age); // * <----------- call
+//   Person.apply(this, [name, age]); // * <----------- apply
+//   const constructer = Person.bind(this); // * <----------- bind
+//   constructer(name, age);
 // 	this.company = company;
 // }
 // Employee.prototype = Object.create(Person.prototype); // chain, inheritance
@@ -257,7 +260,6 @@
 //   console.log(obj[ele]);
 // }
 
-
 // Array.prototype.myForEach = function (cb) {
 //   for (let i = 0; i < this.length; i++) {
 //     cb.call(thisOptions, this[i], i, this);
@@ -269,6 +271,55 @@
 // }, {args: [1, 2, 3]});
 
 // * filter, map, includes, indexOf, myReduce, slice, splice
+// Array.prototype.mySlick = function (start, end) {
+// 	const res = [];
+// 	const len = this.length;
+
+// 	start = start < 0 ? Math.max(len + start, 0) : Math.min(start, len);
+
+//   if (end === undefined || end > len) {
+//     end = len;
+//   } else if (end < 0) {
+//     end = len + end;
+//   } else {
+//     end = end;
+//   }
+
+// 	for (let i = start; i < end; i++) {
+// 		res.push(this[i]);
+// 	}
+
+// 	return res;
+// };
+// Array.prototype.mySplice = function (start, rNum, ...items) {
+// 	const res = [];
+// 	if (start < 0) {
+// 		start = this.length + start;
+// 	}
+// 	for (let i = start; i < start + rNum; i++) {
+// 		res.push(this[i]);
+// 		delete this[i];
+// 	}
+
+// 	const tempArray = [];
+// 	for (let i = 0; i < start; i++) {
+// 		tempArray.push(this[i]);
+// 	}
+// 	for (let i = 0; i < items.length; i++) {
+// 		tempArray.push(items[i]);
+// 	}
+// 	for (let i = start + rNum; i < this.length; i++) {
+// 		tempArray.push(this[i]);
+// 	}
+
+// 	this.length = 0;
+// 	for (let i = 0; i < tempArray.length; i++) {
+// 		this.push(tempArray[i]);
+// 	}
+
+// 	return res;
+// };
+
 // const str = 'abc'; // 'aabbcc';
 // const res = str.split('').reduce(function(acc, cur, i, self) {
 //   acc[cur] = cur;
@@ -293,7 +344,7 @@
 //   arr: ['key', 'val']
 // }
 // // Object.keys(obj).map(ele => {
-  
+
 // // })
 // console.log(Object.keys(obj));
 // console.log(Object.values(obj));
@@ -312,16 +363,198 @@
 // import {Componen: hello, Module: view} from '@angular/core';
 // *
 // * rest parameter vs. spread operator
-(function(a, b, ...n) {
-  console.log(a, b, ...n);
-})(2, 3, 4, 5, 8, 2, 62);
+// (function(a, b, ...n) {
+//   console.log(a, b, ...n);
+// })(2, 3, 4, 5, 8, 2, 62);
 
-const set = new Set([1, 2, 3])
-const arr = [...set];
-console.log(arr);
+// const set = new Set([1, 2, 3])
+// const arr = [...set];
+// console.log(arr);
 
 // const [a, b, ...args] = [2, 3, 4, 5, 8, 2, 62];
 // console.log('args: ', args);
 
-// *
-// * object copy
+// * object copy: shallow, spread operator, JSON.stringify, structuredClone, lib(lodash.cloneDeep)
+// // var foo = function () { return "hi" };
+// const obj = {
+// 	name: "Dio",
+//   arr: [1, 2, 3],
+//   date: new Date(),
+//   foo: function() {
+//     console.log('hello from obj.foo');
+//   }
+// };
+
+// var _ = require('lodash');
+// const obj4 = _.cloneDeep(obj);
+// console.log(obj, obj4);
+// obj4.foo();
+// typeof, Array.isArray
+// const obj1 = {...obj};
+// console.log(JSON.stringify(obj));
+// const obj2 = JSON.parse(JSON.stringify(obj));
+// console.log(obj, obj2);
+
+// const obj3 = structuredClone(obj);
+// console.log(obj.arr[0], obj3.arr[0]);
+// obj.arr[0] = 23;
+// console.log(obj.arr[0], obj3.arr[0]);
+// console.log(obj3);
+
+// // * iife
+
+// ^ interview question~~~~~~~~~~~~~~~
+// const names = [
+// 	{ userid: 2, name: "Velen" },
+// 	{ userid: 56, name: "Illidan" },
+// 	{ userid: 23, name: "Muradin" },
+// 	{ userid: 12, name: "Sylvanas" },
+// 	{ userid: 44, name: "Cenarius" },
+// 	{ userid: 4, name: "Gul'Dan" },
+// ];
+// const roles = [
+// 	{ userid: 2, role: "Mage" },
+// 	{ userid: 4, role: "Worlock" },
+// 	{ userid: 56, role: "Demon Hunter" },
+// 	{ userid: 66, role: "Druid" },
+// 	{ userid: 87, role: "Shaman" },
+// 	{ userid: 12, role: "Hunter" },
+// ];
+// [
+//   {
+//     userid: 2, name:  "Velen", role: "Mage"
+//   },
+//   {
+//     userid: 44, name:  "Cenarius", role: null
+//   },
+//   ...
+//(function(require, export, module....) {})
+
+// * closure
+
+// function foo(a) {
+// 	return function (b) {
+// 		return function(c) {
+//a++;
+//       return a + b + c;
+//     }
+// 	}
+// }
+// const bar = foo(7);
+// bar(1); // [a = 7]
+// bar(7); // [a = 7]
+// bar(2); // [a = 7]
+// bar(1); // [a = 7]
+// bar(98); // [a = 7]
+// bar(3); // [a = 7]
+// bar(6); // [a = 7]
+// console.log(foo(7)(4)(5));
+
+// const callback = function (a, b) {
+// 	return a + b;
+// };
+// const foo = limitedFn(5, callback);
+// const bar = limitedFn(2, callback);
+
+// bar(4, 5); // 9 // [count = 0]
+// bar(4, 8); // 12 // [count = 1]
+// bar(4, 5); // 9 // [count = 2]
+// bar(4, 5); // 9 // [count = 4]
+// bar(4, 5); // 9 // [count = 5]
+// bar(4, 5); // over limited // [count = 5]
+// bar(4, 5); // over limited // [count = 5]
+// bar(4, 5); // over limited // [count = 5]
+
+// foo(4, 5); // 9 // [count = 0]
+// foo(4, 8); // 12 // [count = 1]
+// foo(4, 5); // 9 // [count = 3]
+// foo(4, 5); // 9 // [count = 4]
+// foo(4, 5); // 9 // [count = 5]
+// foo(4, 5); // over limited // [count = 5]
+// foo(4, 5); // over limited // [count = 5]
+// foo(4, 5); // over limited // [count = 5]
+
+// function limitedFn(n, cb) {
+// 	let count = 0;
+
+// 	return function (...args) {
+// 		if (count < n) {
+// 			console.log(cb(...args));
+// 			count++;
+// 		} else {
+// 			console.log("over limited");
+// 		}
+// 	};
+// }
+
+// * currying
+// //^ ~~~~~~interview question~~~~~~~~~~~~
+// const callback1 = (a) => a + 2; // 6
+// const callback2 = (b) => b * 2; // 12
+// const callback3 = (c) => c - 2; // 10
+
+// console.log(runAll(4)(callback1, callback2, callback3)); // 10
+// function runAll(initNum) {
+//   return function(...args) {
+
+//   }
+// }
+
+// * this ---> object; only use it in a function
+// hellofromindex = "David";
+// // console.log('exports: ', this);
+// // console.log(module.exports);
+// // hi = 'hihi';
+// (function() {
+//   console.log('', this.hellofromindex);
+//   console.log(this);
+//   console.log(module.exports);
+// })();
+// (() => {
+//   console.log('', this.hellofromindex);
+// })();
+
+// module.exports = {
+//   name: 'hello',
+// }
+
+// const obj = {
+// 	abcd: "Dio",
+// 	foo() {
+// 		this.hello = "hello";
+// 		console.log("foo: ", this); // <------- this
+
+//     (() => {
+//       console.log("arrow: ", this); // <------- this, same
+//     })();
+
+//     function bar() {
+// 			console.log("function: ", this); // <------- this
+// 		}
+//     const baz = bar.bind(this);
+//     baz();
+// 	},
+// };
+// obj.foo();
+
+// // * call, apply, bind
+// function callName(m, n) {
+// 	// 100
+// 	console.log(this.abcd, m, n);
+// }
+
+// const wrapCallName = callName.bind(obj); // 1
+// // wrapCallName('hello', 'hi'); // Dio hello hi;
+// // module.exports = wrapCallName;
+
+// callName.call(obj, "hello", "hi"); // 1, + 100
+// callName.apply(obj, ["hello", "hi"]); // 1, + 1[100]
+
+// * arrow function
+
+// * event loop
+// * XHR
+// * callback function; callback hell
+// * Promise
+// * MyPromise
+// * MyFetch
---
+++ js/test.js
@@ -0,0 +1,3 @@
+const fn = require('./index');
+
+fn('Tom', 'Helen');

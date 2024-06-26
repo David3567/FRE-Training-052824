0df34a49e7a7eb755cdb0a372db908a93e97e121
lukeyzhong <lukeyzhong@gmail.com>
2024-06-26 12:55:48 -0500

    update

--- rxjs1/src/app/app.component.ts
+++ rxjs1/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { TodoService } from './service/todo.service';
+import { Observable, from, fromEvent, map, of, pipe, take, tap } from 'rxjs';
+import { lookup } from 'dns';
+import { log } from 'console';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   todos: any[] = [];
@@ -12,10 +15,15 @@ export class AppComponent {
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+
+    // new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve('Hello from the promise!');
+    //   }, 2000);
+    // });
+
     this.todoService.getTodos().subscribe(data => {
       this.todos = data;
     });
   }
-
-
 }

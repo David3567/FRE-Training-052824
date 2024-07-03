7474143e701c9500754a5680ce8c2d99ab377c50
Yue Dong <ydong011@fiu.edu>
2024-07-03 09:22:16 -0500

    update

--- Angular/form/src/app/app.component.ts
+++ Angular/form/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'form';
+
+  login(contactForm: NgForm) {
+    console.log(contactForm.value);
+  }
 }
